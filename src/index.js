6d0cd951dd3d42623517c07967da6e362ed98587
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2020-01-08 10:11:38 +0545

    Add working minimal react code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-console.log('Hello World');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import './index.css';
+
+function App() {
+  return <div>Hello World!</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
